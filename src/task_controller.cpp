6dba77d95afafbbb2a49552b59ff60a7e1e16e06
Balázs Gunics <gunicsba@gmail.com>
2026-06-15 19:04:03 +0200

    add more sanitization

--- src/task_controller.cpp
+++ src/task_controller.cpp
@@ -17,6 +17,22 @@
 #include <fstream>
 #include <iostream>
 
+// Sanitize a string for use as a filename by replacing invalid characters with underscores
+static std::string sanitize_filename(const std::string &input)
+{
+	std::string result = input;
+	// Characters invalid on Windows (and / is invalid on POSIX too)
+	const std::string invalid_chars = "\\/*:?\"<>|";
+	for (char &c : result)
+	{
+		if (invalid_chars.find(c) != std::string::npos)
+		{
+			c = '_';
+		}
+	}
+	return result;
+}
+
 void ClientState::set_number_of_sections(std::uint8_t number)
 {
 	numberOfSections = number;
@@ -299,7 +315,7 @@ bool MyTCServer::activate_object_pool(std::shared_ptr<isobus::ControlFunction> p
 		auto it = std::find_if(label.begin(), label.end(), [](unsigned char c) { return c < 0x20 || c >= 0x7F; });
 		label.erase(it, label.end());
 
-		auto fileName = std::to_string(partnerCF->get_NAME().get_full_name()) + "\\" + label + ".ddop";
+		auto fileName = std::to_string(partnerCF->get_NAME().get_full_name()) + "/" + sanitize_filename(label) + ".ddop";
 		std::vector<std::uint8_t> binaryPool;
 		if (state.get_pool().generate_binary_object_pool(binaryPool))
 		{
@@ -508,7 +524,8 @@ void MyTCServer::request_measurement_commands()
 {
 	for (auto &client : clients)
 	{
-		if (!client.second.are_measurement_commands_sent())
+		// Skip clients with 0 sections (e.g. tractors) - sending measurement commands to a tractor ECU can cause unexpected behavior
+		if (!client.second.are_measurement_commands_sent() && client.second.get_number_of_sections() > 0)
 		{
 			// Find all actual (condensed) work state DDIs and request them to trigger "On Change" and "Time Interval"
 			for (std::uint32_t i = 0; i < client.second.get_pool().size(); i++)
